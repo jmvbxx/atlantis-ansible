4c323e70418b7b53db2d8253630a12155fc4f654
Jason Bornhoft <jmvbxx@gmail.com>
2021-06-08 21:44:32 -0500

    Remove unneeded space

--- terraform/state.tf
+++ terraform/state.tf
@@ -47,7 +47,6 @@ resource "aws_s3_bucket_public_access_block" "block" {
   restrict_public_buckets = true
 }
 
-
 resource "aws_dynamodb_table" "terraform-state" {
   name           = "terraform-state"
   read_capacity  = 20
